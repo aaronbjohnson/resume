c005077f0cf6b4f96eed4285ba1f203d264987b3
Aaron <aj65461@gmail.com>
2014-11-07 18:28:09 -0500

    Add style to map

--- js/helper.js
+++ js/helper.js
@@ -109,7 +109,165 @@ function initializeMap() {
   var locations;        
 
   var mapOptions = {
-    disableDefaultUI: true
+    /* Disabled the line below to alow me to add the map style below */
+    /*disableDefaultUI: true*/
+
+    /*
+    Added 'styles' code below to add a style to the map.
+    Website: http://snazzymaps.com/style/111/the-propia-effect
+    Map created by James Wells (http://www.propia.co.uk/)
+    */
+    styles: [
+      {
+        "featureType": "landscape",
+        "stylers": [
+          {
+            "visibility": "simplified"
+          },
+          {
+            "color": "#2b3f57"
+          },
+          {
+            "weight": 0.1
+          }
+        ]
+      },
+      {
+        "featureType": "administrative",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "hue": "#ff0000"
+          },
+          {
+            "weight": 0.4
+          },
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "weight": 1.3
+          },
+          {
+            "color": "#FFFFFF"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f55f77"
+          },
+          {
+            "weight": 3
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f55f77"
+          },
+          {
+            "weight": 1.1
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f55f77"
+          },
+          {
+            "weight": 0.4
+          }
+        ]
+      },
+      {},
+      {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "weight": 0.8
+          },
+          {
+            "color": "#ffffff"
+          },
+          {
+            "visibility": "on"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          },
+          {
+            "weight": 0.7
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "stylers": [
+          {
+            "color": "#6c5b7b"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "stylers": [
+          {
+            "color": "#f3b191"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "stylers": [
+          {
+              "visibility": "on"
+          }
+        ]
+      }
+    ]
   };
 
   // This next line makes `map` a new Google Map JavaScript Object and attaches it to
