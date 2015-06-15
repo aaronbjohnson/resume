2c194042f862f00ff1edb8079b0fe655152bdc78
Aaron <aj65461@gmail.com>
2015-06-15 18:08:55 -0400

    Add chart to use on the new hobbies section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -270,7 +270,7 @@ var education = {
 /**
  * Adding the chart.js stuff here
  */
-
+/*
 var data = [
     {
         value: 300,
@@ -304,6 +304,8 @@ var data = [
     }
 
 ];
+
+
 
 var options = {
 
@@ -355,6 +357,88 @@ var myNewChart = new Chart(ctx).PolarArea(data);
 
 // Draw a polar area chart
 new Chart(ctx).PolarArea(data, options);
+
+*/
+
+// Radar Chart Here:
+
+var data = {
+    labels: ["Painting/Drawing", "Playing Guitar", "Reading", "Gaming"],
+    datasets: [
+        {
+            label: "Hobbies",
+            fillColor: "rgba(90,211,209,0.2)",
+            strokeColor: "#46BFBD",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [90, 60, 80, 25]
+        }
+    ]
+};
+
+var options = {
+    //Boolean - Whether to show lines for each scale point
+    scaleShowLine : true,
+
+    //Boolean - Whether we show the angle lines out of the radar
+    angleShowLineOut : true,
+
+    //Boolean - Whether to show labels on the scale
+    scaleShowLabels : false,
+
+    // Boolean - Whether the scale should begin at zero
+    scaleBeginAtZero : true,
+
+    //String - Colour of the angle line
+    angleLineColor : "rgba(0,0,0,.1)",
+
+    //Number - Pixel width of the angle line
+    angleLineWidth : 1,
+
+    //String - Point label font declaration
+    pointLabelFontFamily : "'Arial'",
+
+    //String - Point label font weight
+    pointLabelFontStyle : "normal",
+
+    //Number - Point label font size in pixels
+    pointLabelFontSize : 10,
+
+    //String - Point label font colour
+    pointLabelFontColor : "#666",
+
+    //Boolean - Whether to show a dot for each point
+    pointDot : true,
+
+    //Number - Radius of each point dot in pixels
+    pointDotRadius : 3,
+
+    //Number - Pixel width of point dot stroke
+    pointDotStrokeWidth : 1,
+
+    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+    pointHitDetectionRadius : 20,
+
+    //Boolean - Whether to show a stroke for datasets
+    datasetStroke : true,
+
+    //Number - Pixel width of dataset stroke
+    datasetStrokeWidth : 2,
+
+    //Boolean - Whether to fill the dataset with a colour
+    datasetFill : true,
+
+    //String - A legend template
+    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+}
+
+var ctx = document.getElementById("myChart").getContext("2d");
+
+var myRadarChart = new Chart(ctx).Radar(data, options);
+
 
 
 
