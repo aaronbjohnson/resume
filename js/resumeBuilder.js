9064711ebd97c3db258ddb1d04e1534e6d62785a
Aaron <aj65461@gmail.com>
2015-06-17 15:15:16 -0400

    Change order of projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -97,22 +97,32 @@ var work = {
 var projects = {
     "projects": [
         {
-            "title" : "HTML and CSS Portfolio",
-            "dates" : "2014",
-            "description": "This was my first project in the Udacity Front-End Web Development Nanodegree. The requirement was to develop a responsive website based on a mockup that would display images, descriptions and links to projects I would be completing during the course of the degree.",
+            "title" : "Feed Reader",
+            "dates" : "2015",
+            "description": "In this project I was given a web-based application that read RSS feeds. The goal was to use Jasmine to write test suites that would test the functionality of the application.",
             "links": [
-                "https://github.com/aj65461/portfolio",
-                "http://aj65461.github.io/portfolio"
+                "https://github.com/aj65461/frontend-nanodegree-feedreader",
+                "http://aj65461.github.io/frontend-nanodegree-feedreader/"
             ],
             "images": []
         },
         {
-            "title" : "Interactive Resume",
-            "dates" : "2014",
-            "description": "This was my second project in the Udacity Front-End Web Development Nanodegree. Here I developed an interactive resume application that would read resume content from a JSON file and would dynamically display that content within a provided template. I made use of objects, functions, conditionals, and control structures to compose the content that would display on the resume.",
+            "title" : "Neighborhood Map",
+            "dates" : "2015",
+            "description": "The goal of this project was to develop a single page application centered around one of my favorite neighborhoods. The map shows markers for my favorite locations in that neighborhood as well as a list view of those locations that can control the display of information about each place. Third-party APIs were used to asynchronously deliver current information about each location. A search and filter function was implemented to display or hide the markers and list items based on matching letters entered into a search bar above the map. I used the Knockout.js framework to keep the UI updated whenever the data model is changed and to connect the Google map markers to their respective list items. I used the Bootstrap framework to make the overall layout responsive on both larger screens and on mobile screens.",
             "links": [
-                "https://github.com/aj65461/resume",
-                "http://aj65461.github.io/resume"
+                "https://github.com/aj65461/frontend-nanodegree-neighborhood-map",
+                "http://aj65461.github.io/frontend-nanodegree-neighborhood-map/"
+            ],
+            "images": []
+        },
+        {
+            "title" : "Mobile Portfolio",
+            "dates" : "2015",
+            "description": "The goal of this project was to analyze the Critical Rendering Path of a provided portfolio page and to make optimizations that would achieve a PageSpeed score of 90 on index.html, ensure a consistent frame rate at 60fps when when scrolling on a page that has a parallax animation in the background, impact content efficiency, and improve time to resize interactive objects on the page to less than 5ms.",
+            "links": [
+                "https://github.com/aj65461/frontend-nanodegree-mobile-portfolio",
+                "http://aj65461.github.io/frontend-nanodegree-mobile-portfolio/"
             ],
             "images": []
         },
@@ -129,32 +139,22 @@ var projects = {
             ]
         },
         {
-            "title" : "Mobile Portfolio",
-            "dates" : "2015",
-            "description": "The goal of this project was to analyze the Critical Rendering Path of a provided portfolio page and to make optimizations that would achieve a PageSpeed score of 90 on index.html, ensure a consistent frame rate at 60fps when when scrolling on a page that has a parallax animation in the background, impact content efficiency, and improve time to resize interactive objects on the page to less than 5ms.",
+            "title" : "Interactive Resume",
+            "dates" : "2014",
+            "description": "This was my second project in the Udacity Front-End Web Development Nanodegree. Here I developed an interactive resume application that would read resume content from a JSON file and would dynamically display that content within a provided template. I made use of objects, functions, conditionals, and control structures to compose the content that would display on the resume.",
             "links": [
-                "https://github.com/aj65461/frontend-nanodegree-mobile-portfolio",
-                "http://aj65461.github.io/frontend-nanodegree-mobile-portfolio/"
+                "https://github.com/aj65461/resume",
+                "http://aj65461.github.io/resume"
             ],
             "images": []
         },
         {
-            "title" : "Neighborhood Map",
-            "dates" : "2015",
-            "description": "The goal of this project was to develop a single page application centered around one of my favorite neighborhoods. The map shows markers for my favorite locations in that neighborhood as well as a list view of those locations that can control the display of information about each place. Third-party APIs were used to asynchronously deliver current information about each location. A search and filter function was implemented to display or hide the markers and list items based on matching letters entered into a search bar above the map. I used the Knockout.js framework to keep the UI updated whenever the data model is changed and to connect the Google map markers to their respective list items. I used the Bootstrap framework to make the overall layout responsive on both larger screens and on mobile screens.",
+            "title" : "HTML and CSS Portfolio",
+            "dates" : "2014",
+            "description": "This was my first project in the Udacity Front-End Web Development Nanodegree. The requirement was to develop a responsive website based on a mockup that would display images, descriptions and links to projects I would be completing during the course of the degree.",
             "links": [
-                "https://github.com/aj65461/frontend-nanodegree-neighborhood-map",
-                "http://aj65461.github.io/frontend-nanodegree-neighborhood-map/"
-            ],
-            "images": []
-        },
-        {
-            "title" : "Feed Reader",
-            "dates" : "2015",
-            "description": "In this project I was given a web-based application that read RSS feeds. The goal was to use Jasmine to write test suites that would test the functionality of the application.",
-            "links": [
-                "https://github.com/aj65461/frontend-nanodegree-feedreader",
-                "http://aj65461.github.io/frontend-nanodegree-feedreader/"
+                "https://github.com/aj65461/portfolio",
+                "http://aj65461.github.io/portfolio"
             ],
             "images": []
         }
